63a39134635b0240fb36d8b436d33c8fa08a5411
Mike Laffy <nhlflash22@gmail.com>
2021-04-24 20:51:59 -0400

    fix type issue

--- walletv2.py
+++ walletv2.py
@@ -12,13 +12,18 @@ from transaction import Transaction
 
 class Wallet:
     def __init__(self, test: bool = False) -> None:
+        self.encrypted_private_key = None  # type: Optional[bytes]
         self.public_key = None  # type: Optional[str]
-        self.private_key = None  # type: RSA.RsaKey
+        self.private_key = None  # type: Optional[RSA.RsaKey]
         self.logged_in = False
         self.address = None  # type: Optional[str]
         if test:
             self.__generate_keys("test")
-            self.private_key = RSA.import_key(self.private_key, passphrase="test")
+            if not self.encrypted_private_key:
+                raise ValueError("Encrypted private key must exist")
+            self.private_key = RSA.import_key(
+                self.encrypted_private_key, passphrase="test"
+            )
             self.address = self.public_key
             self.set_is_logged_in()
 
@@ -34,7 +39,7 @@ class Wallet:
             format="DER",
         )
 
-        self.private_key = encrypted_key
+        self.encrypted_private_key = encrypted_key
         self.public_key = binascii.hexlify(
             key.public_key().export_key(format="DER")
         ).decode("ascii")
@@ -45,13 +50,17 @@ class Wallet:
 
             path = Path("wallet")
             path.mkdir(exist_ok=True)
+            if not self.encrypted_private_key:
+                raise ValueError("Encrypted private key must exist")
             with open(path / ".keys", mode="wb") as f:
-                f.write(self.private_key)
+                f.write(self.encrypted_private_key)
                 self.private_key = RSA.import_key(
-                    self.private_key, passphrase=passphrase
+                    self.encrypted_private_key, passphrase=passphrase
                 )
         except (IOError, IndexError):
             print("Creating login for wallet failed...")
+        except ValueError as e:
+            print(e)
 
         address = self.generate_address()
         self.save_address(address)
@@ -63,10 +72,11 @@ class Wallet:
             path = Path("wallet")
             path.mkdir(exist_ok=True)
             with open(path / ".keys", mode="rb") as f:
-                encrypted_key = RSA.import_key(f.read(), passphrase=passphrase)
-                self.private_key = encrypted_key
+                encrypted_key = f.read()
+                self.encrypted_private_key = encrypted_key
+                self.private_key = RSA.import_key(encrypted_key, passphrase=passphrase)
                 self.public_key = binascii.hexlify(
-                    encrypted_key.public_key().export_key(format="DER")
+                    self.private_key.public_key().export_key(format="DER")
                 ).decode("ascii")
         except (IOError, IndexError):
             print("Retreiving keys from wallet failed...")
@@ -111,6 +121,8 @@ class Wallet:
     # binascii.hexlify is used to convert binary data to hexadecimal representation
     def sign_transaction(self, sender: str, recipient: str, amount: float) -> str:
         print("Signing transaction")
+        if self.private_key is None:
+            raise ValueError("Unable to sign transaction without a private key")
         signer = pkcs1_15.new(self.private_key)
         h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode("utf8"))
         signature = signer.sign(h)
